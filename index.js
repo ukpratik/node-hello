1e8f983e0aac1300818e673788cad491eed6adb2
Pratik Ukey <34273491+ukpratik@users.noreply.github.com>
2022-01-10 18:08:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
+const express = require("express")
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   const msg = 'Hello Node!\n'
+//   res.end(msg);
+// });
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-});
+// server.listen(port, () => {
+//   console.log(`Server running on http://localhost:${port}/`);
+// });
+
+const app = express()
+
+app.listen(port,() => {
+    console.log("Server is up and running.")
+})
